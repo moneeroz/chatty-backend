5f09f1e262a8dcfabbd2cf3949d1be48421966a3
Moneer <moneer.alromimah@gmail.com>
2024-02-08 21:45:46 -0700

    add cors

--- api/core/settings.py
+++ api/core/settings.py
@@ -33,6 +33,7 @@ AUTH_USER_MODEL = "chat.User"
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "daphne",
     "rest_framework",
     "rest_framework_simplejwt",
@@ -74,12 +75,16 @@ CHANNEL_LAYERS = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = "core.urls"
 
